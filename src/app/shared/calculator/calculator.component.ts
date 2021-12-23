da061c8f88cfafe9fcb89e842c2168dde8d84468
Jackson Lacerda <jacksonlacerda2106@gmail.com>
2021-12-23 17:49:12 -0300

    feat: img resolution

--- src/app/shared/calculator/calculator.component.ts
+++ src/app/shared/calculator/calculator.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatorComponent implements OnInit {
 
+  // Interface
+  public result: boolean = false;
+  public background: string = null;
+
   constructor() { }
 
   ngOnInit(): void {
+    // Insert the best image according to screen resolution size
+    if(window.innerWidth > 1900){
+      this.background = '../assets/img/home-largue.jpg';
 
+    }if((window.innerWidth <= 1900) && (window.innerWidth >= 1025)){
+      this.background = '../assets/img/home-medium.jpg';
+
+    }if(window.innerWidth <= 1024){
+      this.background = '../assets/img/home-small.jpg';
+    }
   }
 }
